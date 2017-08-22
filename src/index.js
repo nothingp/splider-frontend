0bc562a7c370e604a5391550d959f26f2add03ff
jundy.chow <zhoujundi711@163.com>
2017-08-22 09:47:36 +0800

    init

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ const app = dva({
   },
 })
 
+require('./mock/common');
+require('./mock/dashboard');
+require('./mock/menu');
+require('./mock/post');
+require('./mock/user');
+
+
 // 2. Model
 app.model(require('./models/app'))
 app.model(require('./models/report'))
@@ -24,3 +31,4 @@ app.router(require('./router'))
 
 // 4. Start
 app.start('#root')
+
